12cfe6e3ea3cd3567b798966259d8541ac32b235
Ancentus <mancentus7@gmail.com>
2023-03-27 19:31:02 +0300

    Solve population equation

--- solve.py
+++ solve.py
@@ -1,44 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def f(x, y):
-    return -y**2 + np.sin(x) # Define the non-linear equation
+# Define the function f(y, t)
+def f(y, t):
+    return 0.2 * y - 0.01 * y**2
 
-def adams_bashforth(f, x0, y0, h, n):
-    x = [x0]
-    y = [y0]
-    for i in range(0, n-1):
-        if i == 0:
-            # Use first-order Euler method to get the second value of y
-            y_next = y[i] + h*f(x[i], y[i])
-        elif i == 1:
-            # Use second-order Adams-Bashforth to get the third value of y
-            y_next = y[i] + (h/2)*(3*f(x[i], y[i]) - f(x[i-1], y[i-1]))
-        elif i == 2:
-            # Use third-order Adams-Bashforth to get the fourth value of y
-            y_next = y[i] + (h/12)*(23*f(x[i], y[i]) - 16*f(x[i-1], y[i-1]) + 5*f(x[i-2], y[i-2]))
-        else:
-            # Use fourth-order Adams-Bashforth to get the fifth and subsequent values of y
-            y_next = y[i] + (h/24)*(55*f(x[i], y[i]) - 59*f(x[i-1], y[i-1]) + 37*f(x[i-2], y[i-2]) - 9*f(x[i-3], y[i-3]))
+# Set the initial conditions
+y0 = 6
+t0 = 2000
+tf = 2020
+h = 1
 
-        # Append x and y to their respective lists
-        x.append(x[i] + h)
-        y.append(y_next)
-    return x, y
+# Compute the number of steps
+N = int((tf - t0) / h)
 
-# Define initial values and parameters
-x0 = 0
-y0 = 1
-h = 0.1
-n = 100
+# Create arrays to store the solution
+t = np.zeros(N+1)
+y = np.zeros(N+1)
+t[0] = t0
+y[0] = y0
 
-# Solve the non-linear equation using Adams-Bashforth method
-x, y = adams_bashforth(f, x0, y0, h, n)
+# Use the Runge-Kutta method to compute the first step
+k1 = h * f(y[0], t[0])
+k2 = h * f(y[0] + 0.5 * k1, t[0] + 0.5 * h)
+k3 = h * f(y[0] + 0.5 * k2, t[0] + 0.5 * h)
+k4 = h * f(y[0] + k3, t[0] + h)
+y[1] = y[0] + (k1 + 2*k2 + 2*k3 + k4) / 6
+t[1] = t[0] + h
+
+# Use the Adams-Bashforth method to compute the remaining steps
+for i in range(2, N+1):
+    # Predictor step
+    y_pred = y[i-1] + h * (3/2 * f(y[i-1], t[i-1]) - 1/2 * f(y[i-2], t[i-2]))
+    t_pred = t[i-1] + h
+    
+    # Corrector step
+    y[i] = y[i-1] + h * (5/12 * f(y_pred, t_pred) + 2/3 * f(y[i-1], t[i-1]) - 1/12 * f(y[i-2], t[i-2]))
+    t[i] = t[i-1] + h
 
 # Plot the solution
-plt.plot(x, y, label='Adams-Bashforth')
-plt.xlabel('x')
+plt.plot(t, y)
+plt.xlabel('t')
 plt.ylabel('y')
-plt.legend()
+plt.title('Population growth over time')
 plt.show()
 
